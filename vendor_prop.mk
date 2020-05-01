d06711ca8b6a6580c779f70f8a1caeff8a7cb9d8
luk1337 <priv.luk@gmail.com>
2020-05-01 11:34:40 +0700

    riva: Enable ro.telephony.use_old_mnc_mcc_format
    Change-Id: I27dff7533d11e3f2ad44c178c2ab7062cb53ae2d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -144,6 +144,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     ro.telephony.default_network=9,1 \
     ro.telephony.call_ring.multiple=false \
+    ro.telephony.use_old_mnc_mcc_format=true \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
